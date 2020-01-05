df6c95be1ff210f346a53f89cbc6865f4ab7b16b
guillermoandrae <guillermo@joinhandshake.com>
2020-01-04 21:50:12 -0500

    Made working examples.

--- examples/tables.php
+++ examples/tables.php
@@ -1,6 +1,6 @@
 <?php
 
-require '../vendor/autoload.php';
+require dirname(__DIR__) . '/vendor/autoload.php';
 
 use Aws\DynamoDb\Marshaler;
 use Guillermoandrae\Db\DynamoDb\AttributeTypes;
@@ -31,11 +31,26 @@ try {
             'keyType' => KeyTypes::RANGE
         ],
     ];
-    $this->useTable($tableName)->createTable($keys);
+
+    if ($adapter->useTable($tableName)->createTable($keys)) {
+        printf("The '%s' table was successfully created!" . PHP_EOL, $tableName);
+    }
+
+    // list the created tables
+    $tables = $adapter->listTables();
+    foreach ($tables as $table) {
+        printf("The '%s' table exists!" . PHP_EOL, $table);
+    }
 
     // delete a table
-    $this->useTable($tableName)->deleteTable();
+    if ($adapter->useTable($tableName)->deleteTable()) {
+        printf("The '%s' table was successfully deleted!" . PHP_EOL, $tableName);
+    }
 
+    // check for the existence of a table
+    if (!$adapter->useTable($tableName)->tableExists()) {
+        printf("The '%s' table no longer exists!" . PHP_EOL, $tableName);
+    }
 } catch(\Exception $ex) {
     die($ex->getMessage());
 }
