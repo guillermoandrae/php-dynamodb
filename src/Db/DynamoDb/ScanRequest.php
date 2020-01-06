47ba1552082921b3404f1c8e3e079d1df782d899
guillermoandrae <guillermo@joinhandshake.com>
2020-01-06 10:30:28 -0500

    ScanFilter is deprecated.

--- src/Db/DynamoDb/ScanRequest.php
+++ src/Db/DynamoDb/ScanRequest.php
@@ -4,23 +4,4 @@ namespace Guillermoandrae\Db\DynamoDb;
 
 final class ScanRequest extends AbstractSearchRequest
 {
-    private $scanFilter = [];
-
-    public function setScanFilter(array $scanFilter): ScanRequest
-    {
-        $this->scanFilter = $scanFilter;
-        return $this;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function get(): array
-    {
-        $query = parent::get();
-        if (!empty($this->scanFilter)) {
-            $query['ScanFilter'] = $this->scanFilter;
-        }
-        return $query;
-    }
 }
