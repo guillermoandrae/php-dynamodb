80b041362f6b53b861fb7b2ba70ac90bfbdd92ab
guillermoandrae <guillermo@joinhandshake.com>
2020-01-08 01:10:58 -0500

    Got rid of an exception case (for now).

--- src/DynamoDb/Operation/DeleteTableOperation.php
+++ src/DynamoDb/Operation/DeleteTableOperation.php
@@ -5,7 +5,6 @@ namespace Guillermoandrae\DynamoDb\Operation;
 use Aws\DynamoDb\Exception\DynamoDbException;
 use Guillermoandrae\DynamoDb\Contract\AbstractTableAwareOperation;
 use Guillermoandrae\DynamoDb\Exception;
-use InvalidArgumentException;
 
 /**
  * @link https://docs.aws.amazon.com/aws-sdk-php/v3/api/api-dynamodb-2012-08-10.html#deletetable
@@ -23,8 +22,6 @@ final class DeleteTableOperation extends AbstractTableAwareOperation
             return true;
         } catch (DynamoDbException $ex) {
             throw new Exception($ex->getMessage());
-        } catch (InvalidArgumentException $ex) {
-            throw new Exception('Bad key schema: ' . $ex->getMessage());
         }
     }
 }
